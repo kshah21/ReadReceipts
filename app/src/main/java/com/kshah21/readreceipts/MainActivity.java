e315d9804e843391b44aaf89f579a363644c5227
KunalS2121 <kshah2121@gmail.com>
2017-06-24 17:57:03 -0500

    Initial Commit with Tess-Two Library ready to use

---
+++ app/src/main/java/com/kshah21/readreceipts/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kshah21.readreceipts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
